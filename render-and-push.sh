2f173b0999322ff024ea962dc34a3436b5bd28d7
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-08-15 01:34:20 -0400

    Auto-commit

--- render-and-push.sh
+++ render-and-push.sh
@@ -1,4 +1,11 @@
 quarto render
+# First the submodule
+cd dsan-globals
+git add -A .
+git commit -m "Auto-commit"
+git push
+# Now the main repo
+cd ..
 git add -A .
 git commit -m "Auto-commit"
 git push
