0017579a0fe06a8112f3bcdc6151f096129b09b5
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-08-22 19:48:22 -0400

    Syllabus update

--- _pre-set-updated.py
+++ _pre-set-updated.py
@@ -5,7 +5,7 @@ if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
   exit()
 
 cur_ts = str(datetime.datetime.now().strftime("%Y-%m-%d, %I:%M:%S %p"))
-env_line = f'DSAN6750_LAST_UPDATED="{cur_ts}"'
+env_line = f'PPOL6805_LAST_UPDATED="{cur_ts}"'
 with open("_environment", 'w', encoding='utf-8') as outfile:
   outfile.write(env_line)
 
