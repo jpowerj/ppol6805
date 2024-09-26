007bb222dc386026d28084a977e23ffdec0771e7
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-09-26 07:48:25 -0400

    Syllabus and resources

--- hello_server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-@app.route("/hack")
-def hacker_detected():
-    return "<p>Hacker detected, pls stop</p>"
